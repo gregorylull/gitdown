3707a1d81cf9e23648c4a9881f56ed0c0d34834e
Hack Reactor <hackreactor@HR-A2.local>
2014-03-31 19:23:30 -0700

    gets previous line of code on ENTER event

--- editorCode.js
+++ editorCode.js
@@ -1,53 +1,101 @@
+
+
 $(function () {
-    var editor = ace.edit("editor");
-    editor.setTheme("ace/theme/monokai");
-    editor.getSession().setMode("ace/mode/javascript");
+  window.editor = ace.edit("editor");
+  editor.setTheme("ace/theme/monokai");
+  editor.getSession().setMode("ace/mode/javascript");
 
-    // setting values: setValue completely replaces
-    // editor.setValue("am i replacing?");
+  // setting values: setValue completely replaces
+  // editor.setValue("am i replacing?");
 
-    // getting values and is just adding to the next. PERFECT!
-    // console.log(editor.getValue());
-    // var text = editor.getValue();
-    // text += "\n var test = 'the next line?';";
+  // getting values and is just adding to the next. PERFECT!
+  // console.log(editor.getValue());
+  // var text = editor.getValue();
+  // text += "\n var test = 'the next line?';";
 
-    // addCommand completely prevents the default
-    editor.commands.addCommand({
-        name: 'unbindP',
-        bindKey: {mac: 'Command-Enter'},
-        exec: function (editor) {
-          console.log(editor.getValue());
-        },
-        readOnly: true
-    });
+  // small window
+  $('#editor').width(450);
+  $('#editor').height(250);
 
-    // $(document).keypress(function(event){
-    //     var keycode = (event.keyCode ? event.keyCode : event.which);
-    //     if (keycode == '13') {
-    //         console.log('enter key was pressed');
-    //         editor.insert('something after enter?');
-    //     }
-    // });
+  /*
+    GIT MESSAGES
+  */
 
-    // new scroll bar?
-    window.$editor = $(editor);
+  var gitIOtext = {
+    "git status": function (gitStatus) {
+        if (true) {
 
-    // editor event binding
-    // on change will trigger for both user AND editor, so maybe use keyup/keydown/press, etc.
-    // on testing, change will not trigger if i inserted
-    editor.getSession().on('change', function(event) {
-        console.log('event: ', event);
-        if (event.data.text === '\n') {
-            console.log('enter key was pressed');
-            console.log('enter key: ', event.data.text.charCodeAt(0));
         }
-    });
+    },
+    clean: "nothing to commit, working directory clean",
+    branchMaster: "# On branch master",
+    notStaged: "# Changes not staged for commit:"
+  };
 
-    // test shows editor cannot handle multiple inputs
-    editor.moveCursorTo(2, 2);
+  var connectMessages = function () {
+    var newMsg = '';
+    for (var i = 0; i < arguments.length; i++) {
+        var msg = arguments[i];
+        empty += '\n' + msg;
+    }
+    return newMsg;
+  };
 
-    editor.insert('a');
-    editor.insert('\n');
+  // get text from previous line, and then returns cursor to original position
+  // *TODO right now pressing enter in the middle of a line will split the line, should not do that
+  var getPreviousLine = function () {
+    editor.selection.moveCursorLineStart();
+    editor.selection.selectLineEnd();
+    var text = editor.session.getTextRange();
+    editor.clearSelection();
+    return text;
+  };
+
+  // for enter key
+  editor.getSession().on('change', (function (e) {
+    // if the enter key is pressed
+    if (e.data.text === '\n') {
+      // check previous line
+      // debugging
+      var command = getPreviousLine();
+      console.log('enter key happend, text: ', command);
+    }
+  }));
+
+  // addCommand completely prevents the default
+  // editor.commands.addCommand({
+  //     name: 'bashEnter',
+  //     bindKey: {mac: 'Enter'},
+  //     exec: function (editor) {
+  //       getPreviousLine(editor);
+  //     },
+  //     readOnly: true
+  // });
+
+  // $(document).keypress(function(event){
+  //     var keycode = (event.keyCode ? event.keyCode : event.which);
+  //     if (keycode == '13') {
+  //         console.log('enter key was pressed');
+  //         editor.insert('something after enter?');
+  //     }
+  // });
+
+  // editor event binding
+  // on change will trigger for both user AND editor, so maybe use keyup/keydown/press, etc.
+  // on testing, change will not trigger if i inserted
+  // editor.getSession().on('change', function(event) {
+  //     console.log('event: ', event);
+  //     if (event.data.text === '\n') {
+  //         console.log('enter key was pressed');
+  //         console.log('enter key: ', event.data.text.charCodeAt(0));
+  //     }
+  // });
+
+  // // test shows editor cannot handle multiple inputs
+  // editor.moveCursorTo(2, 2);
+
+  // editor.insert('a');
+  // editor.insert('\n');
 
 // end of document.ready
 });
